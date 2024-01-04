4a529efa9c5fd12d1ec54c4ec31db620bb23a9de
mazrean <shunsuke.w04@gmail.com>
2024-01-04 15:29:07 +0900

    katex対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from "astro/config";
-import yaml from "@rollup/plugin-yaml";
-
-import alpinejs from "@astrojs/alpinejs";
+import { defineConfig } from 'astro/config'
+import yaml from '@rollup/plugin-yaml'
+import alpinejs from '@astrojs/alpinejs'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   vite: {
     plugins: [yaml()]
   },
-  integrations: [alpinejs()]
-});
+  integrations: [alpinejs()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }
+})
